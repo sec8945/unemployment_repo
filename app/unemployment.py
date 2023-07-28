69359df4c051bdd59ca43edb53fd0fcc91a3f6d5
sec8945 <sec8945@stern.nyu.edu>
2023-07-27 20:41:29 -0400

    passing api key as environment variable

--- app/unemployment.py
+++ app/unemployment.py
@@ -1,9 +1,14 @@
-print('Hello')
-from getpass import getpass
 
-API_KEY = getpass("Please input your AlphaVantage API Key: ")
+#from getpass import getpass
+#
+#API_KEY = getpass("Please input your AlphaVantage API Key: ")
 
+import os
+from dotenv import load_dotenv
 
+load_dotenv() #> invoking this function loads contents of the ".env" file into the script's environment...
+
+API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
 import requests
 import json
 from pprint import pprint
